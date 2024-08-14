5166c61a968dc23dfebedb26aeafd0cad631d463
snicker <ngordon779@gmail.com>
2024-08-14 03:53:03 +0000

    adjust altitude sensor unit to centimeters instead of meters. closes #49

--- custom_components/zwift/sensor.py
+++ custom_components/zwift/sensor.py
@@ -99,7 +99,7 @@ SENSOR_TYPES = {
     'speed': {'name': 'Speed', 'unit': 'mph', 'unit_metric': 'kmh', 'icon': 'mdi:speedometer'},
     'cadence': {'name': 'Cadence', 'unit': 'rpm', 'icon': 'mdi:rotate-right'},
     'power': {'name': 'Power', 'unit': 'W', 'icon': 'mdi:flash'},
-    'altitude': {'name': 'Altitude', 'unit': 'ft', 'unit_metric': 'm', 'icon': 'mdi:altimeter'},
+    'altitude': {'name': 'Altitude', 'unit': 'ft', 'unit_metric': 'cm', 'icon': 'mdi:altimeter'},
     'distance': {'name': 'Distance', 'unit': 'miles', 'unit_metric': 'm', 'icon': 'mdi:arrow-expand-horizontal'},
     'gradient': {'name': 'Gradient', 'unit': '%', 'icon': 'mdi:image-filter-hdr'},
     'level': {'name': 'Level', 'icon': 'mdi:stairs'},
